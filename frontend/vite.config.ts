8d1aca44ab7b9138517df200ea59d050bfd7a609
Gary Johnson <jazzbassjohnson@gmail.com>
2024-07-23 13:37:30 -0700

    Setup docker, django, and postgres

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': 'http://localhost:8000'
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
